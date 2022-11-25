692c5d9c14d85c69687c4cfd19db35528eed817a
Ismael Morales Garcias <zs19017421@estudiantes.uv.mx>
2022-11-24 22:47:07 -0600

    decima primera subida 24/11/22

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -4,12 +4,14 @@ import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
+import { MultipleCustomHooks } from './03-examples'
+import { MemoHook } from './06-memos/MemoHook'
 //import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MemoHook />
   </React.StrictMode>
 )
